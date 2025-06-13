620e8bb81a26139818f6e1b8ebaa5f4137f1cbef
gustavo <67645041g@gmail.com>
2025-06-13 01:40:04 -0400

    otro funcional

--- train.py
+++ train.py
@@ -1,11 +1,21 @@
 from stable_baselines3 import PPO
+from stable_baselines3.common.vec_env import DummyVecEnv, VecNormalize
 from env import SecuenciamientoEnv
 
 def train():
-    env = SecuenciamientoEnv()
-    model = PPO("MlpPolicy", env, verbose=1)
-    model.learn(total_timesteps=10000)
+    # Crear entorno vectorizado y normalizado
+    env = DummyVecEnv([lambda: SecuenciamientoEnv()])
+    env = VecNormalize(env, norm_obs=True)
+
+    # Crear modelo con logging
+    model = PPO("MlpPolicy", env, verbose=1, tensorboard_log="./ppo_logs/")
+
+    # Entrenar el modelo
+    model.learn(total_timesteps=100000)
+
+    # Guardar el modelo y entorno
     model.save("modelo_secuenciamiento")
+    env.save("env_normalizado.pkl")
 
 if __name__ == "__main__":
     train()
